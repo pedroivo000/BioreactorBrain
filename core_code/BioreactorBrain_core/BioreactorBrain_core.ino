c6c8aec23d9eaba2f7f8c9d3d034925113f9778d
Grad <grad@Dentons-MBP.bad>
2016-09-21 23:44:21 -0400

    Added heating pad control using relay board

--- core_code/BioreactorBrain_core/BioreactorBrain_core.ino
+++ core_code/BioreactorBrain_core/BioreactorBrain_core.ino
@@ -2,14 +2,14 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <SD.h>
-
+#include <Ethernet.h>
 ///////////////////////////////// VARIABLE DECLARATION AND LIBRARY SETUP ////////////////////////////////////////////
 //Initialize the LCD library with the numbers of the interface pins:
-LiquidCrystal lcd(2,3,4,5,6,7); //LCD pin name: RS, E, DB4, DB5, DB6, DB7
+LiquidCrystal lcd(19,18,17,16,15,14); //LCD pin name: RS, E, DB4, DB5, DB6, DB7
 
 //Initialize pins:
 const int tempProbe = A0;
-byte padRelayPin = 9;
+byte padRelayPin = 22;
 
 //Log file:
 File logFile;
@@ -26,23 +26,34 @@ byte sensor_bytes_received = 0;       // We need to know how many characters byt
 byte code = 0;                        // used to hold the I2C response code.
 byte in_char = 0;                     // used as a 1 byte buffer to store in bound bytes from the I2C Circuit.
 
+//Initialize webserver: 
+// Enter a MAC address for your controller below.
+byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
+EthernetServer server = EthernetServer(80);
+
+//Time keeping:
+unsigned long previousMillis = 0;
+const long measureInterval = 5000;
+
 void setup() {
   pinMode(padRelayPin, OUTPUT);
 
   Serial.begin(9600);
   Wire.begin(); //Initialize I2C bus as slave
   
-  // set up the LCD:
+  //set up the LCD:
   lcd.begin(20, 4);
-  lcd.print("BioreactorBrain v1.0");
-
+  lcd.print("BioreactorBrain v2.0");
   //Showing the user if everything is going ok during initialization:
   lcd.setCursor(0,1);
-  lcd.print("SD card...");
+  lcd.print("1. SD card...");
   
   //Initializing SD card:
-  //Check if card is present and can be used:
-  if(!SD.begin(10)){
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH); //turning ethernet off for just a little bit
+  
+  //1.Check if card is present and can be used:
+  if(!SD.begin(4)){
     lcd.setCursor(10,1);
     lcd.print(": X");
     return; //do nothing else
@@ -51,7 +62,7 @@ void setup() {
     lcd.print(": OK");
   }
 
-  //Creating log file: 
+  //2.Creating log file: 
   for (uint8_t i = 0; i < 100; i++) {
     fileName[6] = i/10 + '0';
     fileName[7] = i%10 + '0';
@@ -69,38 +80,25 @@ void setup() {
     lcd.print("Log = ");
     lcd.print(logFile.name());  
   }
+
+  logFile.close(); //We need to close the file here so we can log the data and 
+                   //use the webserver capabilities later in the code. 
+
+  //Initializing ethernet connection:
+  lcd.setCursor(0,3);
+  lcd.print("2. Internet..."); 
+  if(Ethernet.begin(mac) == 0) {
+    lcd.setCursor(11,3);
+    lcd.print(": X");
+    return;
+  } else{
+    lcd.setCursor(11,3);
+    lcd.print(": OK");
+    server.begin();
+  }
   delay(5000);
   lcd.clear();
-  lcd.print("Sensor readings:"); 
-
-  pinMode(10, OUTPUT);  
-
-  //Initializing SD card:
-  digitalWrite(10, HIGH); //turn ethernet off on the SPI bus
-  Serial.print("SD card...");
-  //Check if card is present and can be used:
-  if(!SD.begin(4)){
-    Serial.println(": X");
-    return; //do nothing else
-  }else{
-    Serial.println(": OK");
-  }
-
-  //Creating log file: 
-  for (uint8_t i = 0; i < 100; i++) {
-    fileName[6] = i/10 + '0';
-    fileName[7] = i%10 + '0';
-    if (SD.exists(fileName)) {
-      continue;
-    }
-    logFile = SD.open(fileName, FILE_WRITE);
-    break;
-  }
-  if(!logFile){
-    Serial.println("Log file error!");
-  }else{
-    Serial.println(logFile.name());
-  }
+  lcd.print("Sensor readings:");
 }
 
 void loop() {
@@ -109,52 +107,118 @@ void loop() {
   unsigned long currentMillis = millis(); //current time since beggining of run
   
   /////////////////////////// DATA COLLECTION /////////////////////////////////
-  //Getting temperature from sensor:
-  temp = getTemp(tempProbe);
-  //Using this temperature to correct the readings from the DO and ph probes:
-  I2CtempCompensation(temp);
+  if(currentMillis - previousMillis >= measureInterval) {
+    previousMillis = currentMillis;
+    //Getting temperature from sensor:
+    temp = getTemp(tempProbe);
+    //Using this temperature to correct the readings from the DO and ph probes:
+    I2CtempCompensation(temp);
   
-//  //Blink read indicator LED:
-//  if(probeLedState == LOW) {
-//    probeLedState = HIGH;
-//    digitalWrite(probeLedPin, probeLedState);
-//  } else {
-//    probeLedState = LOW;
-//    digitalWrite(probeLedPin, probeLedState);
-//  }
-
-  //Append temp to data logging string:
-  dataString = String(currentMillis/1000) + "," + String(temp);
-  //I2C data collection:
-  dataString += getI2Cdata();
-  
-  //Writing to log file: 
-  Serial.println(dataString);
-  logFile.println(dataString);
-  logFile.flush();
-
-  //Print to LCD screen 
-  lcd.setCursor(0, 1);
-  //Printing the DO and pH probe data:
-  for (byte i = 0; i<TOTAL_CIRCUITS; i++){
-    lcd.print(probe_names[i]);
-    lcd.print(" ");
-    lcd.print(sensorData[i]);
-    lcd.print("  ");
-  }
-
-  //Printing temperature:
-  lcd.setCursor(0,2);
-  lcd.print("T ");
-  lcd.print(temp);
-
-  //////////////////////////// REACTOR CONTROL ////////////////////////////////
-  //Heating pad control:
-  if(temp < 37){
-      digitalWrite(padRelayPin, HIGH); //turn pad to heat water
-    }else if (temp >37) {
-      digitalWrite(padRelayPin, LOW); //shut pad off when desired temperature is reached
+    //Append temp to data logging string:
+    dataString = String(currentMillis/1000) + "," + String(temp);
+    //I2C data collection:
+    dataString += getI2Cdata();
+    
+    //Writing to log file: 
+//    Serial.println(dataString);
+    logFile = SD.open(fileName, FILE_WRITE);
+    if(logFile){
+      logFile.println(dataString);
+      logFile.close();
     }
+  
+    //Print to LCD screen 
+    lcd.setCursor(0, 1);
+    //Printing the DO and pH probe data:
+    for (byte i = 0; i<TOTAL_CIRCUITS; i++){
+      lcd.print(probe_names[i]);
+      lcd.print(" ");
+      lcd.print(sensorData[i]);
+      lcd.print("  ");
+    }
+    //Printing temperature:
+    lcd.setCursor(0,2);
+    lcd.print("T ");
+    lcd.print(temp);
+
+    //////////////////////////// REACTOR CONTROL ////////////////////////////////
+    //Heating pad control:
+    if(temp < 30){
+        digitalWrite(padRelayPin, LOW); //turn pad to heat water
+    }else if (temp >30) {
+        digitalWrite(padRelayPin, HIGH); //shut pad off when desired temperature is reached
+    }  
+  } 
+  else {
+    ////////////////////////////// WEB SERVER //////////////////////////////////
+    //Webserver stuff (modified from: http://www.ladyada.net/learn/arduino/ethfiles.html)
+    char clientline[100];
+    int index = 0;
+    
+    //listen for incoming clients:
+    EthernetClient client = server.available();
+    if (client) {
+      //First, we need to close the logfile: 
+      
+      // an http request ends with a blank line
+      boolean current_line_is_blank = true;
+    
+      // reset the input buffer
+      index = 0;
+    
+      while (client.connected()) {
+        if (client.available()) {
+          char c = client.read();
+          // If it isn't a new line, add the character to the buffer
+          if (c != '\n' && c != '\r') {
+            clientline[index] = c;
+            index++;
+            continue;
+          }
+          // got a \n or \r new line, which means the string is done
+          clientline[index] = 0;
+          // Print it out for debugging
+          Serial.println(clientline);
+          // Look for substring such as a request to get the root file
+          if (strstr (clientline, "GET / ") != 0) {
+            // print all the files:
+            client.println("Files:");
+            ListFiles(client);
+          } 
+          else if (strstr (clientline, "GET /") != 0) {
+            // this time no space after the /, so a sub-file!
+            char *filename;
+            filename = clientline + 5; // look after the "GET /" (5 chars)
+            // a little trick, look for the " HTTP/1.1" string and 
+            // turn the first character of the substring into a 0 to clear it out.
+            (strstr (clientline, " HTTP"))[0] = 0;
+            // print the file we want
+            Serial.println(filename);
+    
+            //Opening file: 
+            File file = SD.open(filename);
+            if (file) {
+              Serial.println("Opened!");
+              int16_t c;
+              while ((c = file.read()) > 0) {
+                // uncomment the serial to debug (slow!)
+                Serial.print((char)c);
+                client.print((char)c);
+            }
+            file.close();
+            }else{
+              Serial.println("File not found!");  
+              client.println("File Not Found!");
+            }
+          }
+          break;
+        }
+      }
+      // give the web browser time to receive the data
+      delay(1);
+      client.stop();
+    }
+  }
 }
 
 ///////////////////////////////// FUNCTIONS ///////////////////////////////////
@@ -237,3 +301,25 @@ void I2CtempCompensation(float temp) {
     delay(300);
   }
 }
+
+void ListFiles(EthernetClient client) {
+  File root = SD.open("/");
+  root.rewindDirectory();
+  while (true) {
+    File entry =  root.openNextFile();
+    if (!entry) {
+      // no more files
+      break;
+    }
+    if (entry.isDirectory()) {
+      continue;
+    } else {
+      // files have sizes, directories do not
+      client.print(entry.name());
+      client.print("\t");
+      client.println(entry.size(), DEC);
+    }
+    entry.close();
+  }
+}
+
